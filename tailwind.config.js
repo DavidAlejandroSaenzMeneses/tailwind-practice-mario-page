1fa2aa674c11974cca9019372d3be5ceca5c94a8
DavidAlejandroSaenzMeneses <dasm17.as@gmail.com>
2022-02-04 15:48:30 -0500

    Parte Uno Finalizada
    Se termina el diseño de la pagina estatica basada en un juego de Mario Bros

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,17 @@ module.exports = {
     theme: {
         extend: {
             backgroundImage: {
-                'wave-pattern': "url('/img/wave-white.png')",
+                'wave-pattern': "url('../../src/assets/img/wave-white.png')",
+                'wave-pattern-dots': "url('../../src/assets/img/pattern-white-dots.png')",
+                'pattern-characters-red': "url('../../src/assets/img/pattern-characters-red.png')",
+                'character-mario': "url('../../src/assets/img/character-l.png')",
+                'wave-pink': "url('../../src/assets/img/wave-pink.png')",
+                'yellow-dots': "url('../../src/assets/img/pattern-yellow-dots.png')",
+            },
+            screens: {
+                'xsm': '320px'
             }
         }
     },
     plugins: [],
-  }
+}
